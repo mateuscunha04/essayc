b00a27c5adb2ef54e753b6a96b855b3557deb50d
Mateus Cunha <125688445+mateuscunha04@users.noreply.github.com>
2023-09-25 06:25:42 -0300

    final input (I think so)

--- essay.c
+++ essay.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
 #include <string.h>
-#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
 
 char guess[50]; 
-
-
-int desenho();
+int randomNumber;
+void desenhoDog();
+void desenhoMouse();
+void desenhoMamute();
 
 int main() {
-    setlocale(LC_ALL, "Portuguese_Brazil.1252");
-    printf("\t Tenho patas e sou muito brincalhão, que animal eu sou?");
-    scanf("%s", guess); // Use %s para ler uma string
+    srand(time(NULL));
+    randomNumber = rand() % 3;
 
-    // Use strcmp para comparar strings
-    if (strcmp(guess, "cachorro") == 0) {
-        printf("Você acertou!\n");
-        desenho();
-    } else {
-        printf("Você errou, tente novamente.");
-    }
+    void (*charadaArray[3])();
+
+    charadaArray[0] = desenhoDog;
+    charadaArray[1] = desenhoMouse;
+    charadaArray[2] = desenhoMamute;
+
+    charadaArray[randomNumber]();
 
     return 0;
 }
 
-int desenho() {
-    printf("     / \\__\n");
-    printf("    (    @\\___\n");
-    printf("    /         O\n");
-    printf("   /   (_____/\n");
-    printf("  /_____/   U\n");
+void desenhoDog() {
+    printf("\t Tenho patas e sou muito brincalhao, que animal eu sou? \t");
+    scanf("%s", guess);
 
-    return 0; 
+    if (strcmp(guess, "cachorro") == 0) {
+        printf("Voce acertou!\n\n");
+        printf("     / \\__\n");
+        printf("    (    @\\___\n");
+        printf("    /         O\n");
+        printf("   /   (_____/\n");
+        printf("  /_____/   \n"); 
+    } else {
+        printf("Voce errou, tente novamente.");
+    }
+}
+
+void desenhoMouse() {
+    printf("\t Tenho a mania de roer as coisas e as pessoas tem medo de mim. \t");
+    scanf("%s", guess);
+
+    if (strcmp(guess, "rato") == 0) {
+        printf("Voce acertou!\n\n");
+        printf("      ,  ,\n");
+        printf("    <:3  )~\n");
+        printf("      '  '\n");
+    } else {
+        printf("Voce errou, tente novamente.");
+    }
+}
+
+void desenhoMamute() {
+    printf("\t Vivi em temperaturas congelantes e ja estou extinto. \t");
+    scanf("%s", guess);
+
+    if (strcmp(guess, "mamute") == 0) {
+        printf("Voce acertou!\n\n");
+        printf("      __  __\n");
+        printf("     /o o\\  \\ \n");
+        printf("    ( J J _  \\  \n");
+        printf("     W  W   W W \n");
+    } else {
+        printf("Voce errou, tente novamente.");
+    }
 }
